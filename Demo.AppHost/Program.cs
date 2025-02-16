e6cff3c2de299cbd840fb33fd2d98c7117c9d00e
Christian Schindlbeck <c.schindlbeck@outlook.de>
2025-02-16 10:53:14 +0100

    Added database, cache, transit and logging sink

--- Demo.AppHost/Program.cs
+++ Demo.AppHost/Program.cs
@@ -1,10 +1,20 @@
+using Aspire.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.Demo_ApiService>("apiservice");
+var cache = builder.AddRedis("servicecache");
+var messageBus = builder.AddRabbitMQ("messagebus").WithLifetime(ContainerLifetime.Persistent);
+var logging = builder.AddSeq("logging");
+var userDb = builder.AddPostgres("db").WithPgAdmin();
 
 builder.AddProject<Projects.Demo_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService)
+    .WithReference(cache)
+    .WithReference(userDb)
+    .WithReference(messageBus)
+    .WithReference(logging)
     .WaitFor(apiService);
 
 builder.Build().Run();
